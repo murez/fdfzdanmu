199e1e8ea75592014f18b1fb4ce3437d7739889f
ottozsy <ottozsy@outlook.com>
2017-05-27 14:30:02 +0800

    F2

---
+++ index.js
@@ -0,0 +1,60 @@
+var express = require('express')
+var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var danmucolors = [{'color':'red','name':'红','default':true},{'color':'orange','name':'橙'},{'color':'blue','name':'蓝'},{'color':'grey','name':'灰'},{'color':'white','name':'白'},{'color':'black','name':'黑'},{'color':'yellow','name':'黄'}]
+var danmusizes = [{'num':0,'name':'小字'},{'num':1,'name':'大字','default':true}]
+var danmuposition = [{'num':0,'name':'飞行','default':true},{'num':1,'name':'顶部'},{'num':2,'name':'底部'}]
+//var danmuspeed = [{'speed':10,'name':'2倍速'},{'speed':40,'name': '1倍速','default':true}]
+// config
+app.locals.title = 'Danmu Slide Show';
+app.locals.email = 'Ott0';
+app.set('view engine', 'jade');
+app.set('views', './views');
+app.use(express.static('public'));
+
+// router
+app.get('/', function(req, res){
+  res.render('slidelist',{slides:[{'tplfile':'2015-07-16-example','date':'2015-07-16','slidename':'示例'}]});
+});
+app.get('/danmu/:id', function (req, res, next) {
+  console.log('Request page [danmu] :', req.params.id);
+  res.render('layouts/danmu', { slidefilename: req.params.id});
+});
+app.get('/slide/:id', function (req, res, next) {
+  console.log('Request page [slide] :', req.params.id);
+  res.render('pages/'+req.params.id);
+});
+app.get('/danmuup',function (req, res, next) {
+	console.log('A Updanmu client page requested.');
+	res.render('layouts/danmuup',{ colors :danmucolors, sizes :danmusizes, positions :danmuposition})
+})
+// socket io
+var status = {'onlinenum':0,'danmunum':0}
+
+io.on('connection', function(socket){
+  // 上线
+  console.log('A user connected: usernum('+(++status['onlinenum'])+') ');
+  // 注销
+  socket.on('disconnect', function(){
+    console.log('A user disconnected: usernum('+(--status['onlinenum'])+') ');
+  });
+  // 客户端上传弹幕-- 用于发射
+  socket.on('updanmu',function(msg) {
+  	console.log('A danmu received('+(++status['danmunum'])+'):'+msg);
+  	// 弹幕广播
+  	io.sockets.emit('broadcastdanmu',msg);
+
+  })
+});
+
+
+// server
+var server = http.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
---
+++ public/js/danmu.js
@@ -0,0 +1,113 @@
+﻿// 全局量
+var socket = io();// socket.io obj
+var canfocus = true;// auto focus on slide ifream when not focus on danmu config
+var winWidth,winHeight;
+
+function initElementSize () {
+      if (window.innerWidth)
+            winWidth = window.innerWidth;
+      else if ((document.body) && (document.body.clientWidth))
+            winWidth = document.body.clientWidth;
+      // 获取窗口高度
+      if (window.innerHeight)
+            winHeight = window.innerHeight;
+      else if ((document.body) && (document.body.clientHeight))
+            winHeight = document.body.clientHeight;
+      $("#fream").css("height",winHeight);
+      $("#danmuconfig").css("width",winWidth);
+}
+function focusOnSlide() {
+      if (canfocus)$("#fream").focus();
+      setTimeout(focusOnSlide,1500);
+};
+$(function () {
+      initElementSize();
+      focusOnSlide();
+      // banding danmu functions
+      // change alpha
+      $("#alphabar").change(function () {
+            $("#alphatext").html($(this).val()+"%");
+            $("#danmu").data("opacity",($(this).val())/100.0);
+      })
+      // danmu show config bar
+      $(document).mousemove(function (e) {
+            if(e.pageY>40){
+                  $("#fream").focus();
+                  $("#danmuconfig").fadeOut();
+                  canfocus = true;
+            }else{
+                  $("#danmuconfig").fadeIn();
+                  canfocus = false;
+            }
+      });
+
+
+    var p1={ "text":"这个弹幕逼格好高啊" , "color":"green" ,"size":"2","position":"0","time":80};
+    var p1____={ "text":"这个弹幕逼格好高啊" , "color":"green" ,"size":"2","position":"0","time":95};
+    var a_danmu={ "text":"欢迎来到复旦附中青浦分校2017年第二届青春节 ~~~~开启弹幕系统~~~~ ~~开启完成~~ (゜-゜)つロ~  " , "color":"green" ,"size":"2","position":"0","time":90};
+    var b_danmu={"text":"██████████████████████████████████████████████████████████","color":"black","size":"1","position":"1","time":20};
+    var c_danmu={"text":"█████████ _____________████ _____________████ _____________████████████████████","color":"black","size":"1","position":"1","time":20};
+    var d_danmu={"text":"█████████ _██████████ _██████████ _██████████████████████████","color":"black","size":"1","position":"1","time":20};
+      $("#danmu").danmu({
+            left: 0,    //区域的起始位置x坐标
+            top: 20 ,  //区域的起始位置y坐标
+            height: winHeight-20, //区域的高度
+            width: winWidth, //区域的宽度
+            zindex :100, //div的css样式zindex
+            speed:30000, //弹幕速度，飞过区域的毫秒数
+            sumtime:60*60*10 , //弹幕运行总时间
+            default_font_color:"#000000", //弹幕默认字体颜色
+            font_size_small:24, //小号弹幕的字体大小,注意此属性值只能是整数
+            font_size_big:32, //大号弹幕的字体大小
+            opacity:"0.9", //弹幕默认透明度
+            top_botton_danmu_time:6000 //顶端底端弹幕持续时间
+      });
+    function FFF() {
+
+        $('#danmu').danmu("add_danmu", a_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+        $('#danmu').danmu("add_danmu", c_danmu);
+        $('#danmu').danmu("add_danmu", c_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", c_danmu);
+        $('#danmu').danmu("add_danmu", c_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", d_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+        $('#danmu').danmu("add_danmu", b_danmu);
+    }
+
+    $('#danmu').danmu('danmu_start');
+
+    FFF;
+    $('#danmu').danmu("add_danmu", p1____);
+    function warm_up()
+    {
+
+    }
+
+      // socket io
+
+      socket.on('broadcastdanmu', function(msg){
+            console.log('get danmu'+msg);
+            msg['time'] = $('#danmu').data("nowtime");
+            $('#danmu').danmu("add_danmu",msg);
+      });
+    for (var i=0;i<cars.length;i++)
+    {
+        $('#danmu').danmu("add_danmu", p1);
+    }
+})
